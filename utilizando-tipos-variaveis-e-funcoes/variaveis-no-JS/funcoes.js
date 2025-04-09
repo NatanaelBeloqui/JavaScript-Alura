ab051277aad74ddc90c8b984d71b542d36e808d9
Natanael Beloqui de Barros <nbdb2005@gmail.com>
2025-04-09 15:11:02 -0300

    Funções

---
+++ utilizando-tipos-variaveis-e-funcoes/variaveis-no-JS/funcoes.js
@@ -0,0 +1,11 @@
+// Parâmetros/Argumentos
+// retorno
+
+function exibeInfosEstudante(nome, nota) {
+    return `O nome é ${nome} e a nota é ${nota}`;
+}
+
+console.log(exibeNomeEstudante('Ísis', 10));
+exibeNomeEstudante('Ana', 9);
+
+// Os parâmetros precisam ser passados na mesma ordem que estão na função
