1a86dfcb3b244005ec19a55ac0e43410617b6f71
Natanael Beloqui de Barros <nbdb2005@gmail.com>
2025-04-09 14:18:53 -0300

    Tipo Null e Undefined

---
+++ utilizando-tipos-variaveis-e-funcoes/variaveis-no-JS/tipo-null-undefined.js
@@ -0,0 +1,10 @@
+let nomeEstudante;
+const professora = 'Ana';
+
+console.log(nomeEstudante); // Undefined
+console.log(typeof nomeEstudante); // Typeof mostra o tipo da variável. Nesse caso será undefined.
+
+let telefoneEstudante = null; // Null
+
+console.log(telefoneEstudante + 3); // Resultado é 3
+console.log(nomeEstudante + 3); // Not An Number -> NaN
