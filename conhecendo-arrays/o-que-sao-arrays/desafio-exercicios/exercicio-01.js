0e4d914e48f318fb1906fb19b03381d4d0312688
Natanael Beloqui de Barros <nbdb2005@gmail.com>
2025-04-12 02:34:09 -0300

    Exercícios em andamento

---
+++ conhecendo-arrays/o-que-sao-arrays/desafio-exercicios/exercicio-01.js
@@ -0,0 +1,3 @@
+const arrayEsparso = [,1,,3,,,,7,,,];
+
+console.log(`${arrayEsparso} ->`, arrayEsparso.length);
---
+++ conhecendo-arrays/o-que-sao-arrays/desafio-exercicios/exercicio-02.js
@@ -0,0 +1,5 @@
+const array = [0,1,2,3,4];
+console.log(array);
+
+array[0]=5;
+console.log(array);
