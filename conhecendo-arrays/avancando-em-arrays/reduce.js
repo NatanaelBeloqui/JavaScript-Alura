6359a12dafd979f88f0451af80d74b11603f34d8
Natanael Beloqui de Barros <nbdb2005@gmail.com>
2025-04-14 15:00:00 -0300

    docs: adiciona comentários explicativos no código

--- conhecendo-arrays/avancando-em-arrays/reduce.js
+++ conhecendo-arrays/avancando-em-arrays/reduce.js
@@ -3,10 +3,7 @@ const salaJava = [6, 5, 8, 9, 5, 6];
 const salaPython = [7, 3.5, 8, 9.5];
 
 function calculaMedia(listaDeNotas) {
-    // const somaDasNotas = listaDeNotas.reduce((acumulador, nota) => {
-    //     return acumulador + nota;
-    // }, 0);
-
+    //dois parâmetros -> O primeiro é o acumudalor e o segundo e é valor inicial do acumulador
     const somaDasNotas = listaDeNotas.reduce((acumulador, nota) => acumulador + nota, 0);
 
     const media = somaDasNotas / listaDeNotas.length;
