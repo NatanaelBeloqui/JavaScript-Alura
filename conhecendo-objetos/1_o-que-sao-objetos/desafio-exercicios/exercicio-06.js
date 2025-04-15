80f5fada22c56234a96cac9df54bbd8c722ef33f
Natanael Beloqui de Barros <nbdb2005@gmail.com>
2025-04-15 15:04:21 -0300

    feat: deleta uma propriedade do objeto

---
+++ conhecendo-objetos/1_o-que-sao-objetos/desafio-exercicios/exercicio-06.js
@@ -0,0 +1,14 @@
+const anoAtual = new Date().getFullYear();
+
+const livro = {
+    titulo: "O Senhor dos Anéis",
+    autor: "J.R.R. Tolkien",
+    anoPublicacao: 1954,
+    genero: "Fantasia",
+    idadePublicacao: anoAtual - 1954,
+    avaliacao: null
+};
+
+delete livro.avaliacao;
+
+console.log(livro);
