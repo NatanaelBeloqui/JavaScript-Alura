e920ebfd340098102f6507cda46854887d2a9ac0
Natanael Beloqui de Barros <nbdb2005@gmail.com>
2025-04-15 15:02:04 -0300

    feat: altera uma propriedade do objeto

---
+++ conhecendo-objetos/1_o-que-sao-objetos/desafio-exercicios/exercicio-05.js
@@ -0,0 +1,13 @@
+const anoAtual = new Date().getFullYear();
+
+const livro = {
+    titulo: "O Senhor dos Anéis",
+    autor: "J.R.R. Tolkien",
+    anoPublicacao: 1954,
+    genero: "Fantasia",
+    idadePublicacao: anoAtual - 1954,
+};
+
+livro.genero = 'Aventura';
+
+console.log(livro);
