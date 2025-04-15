6aee6ac79fe91a9ce206e82d25948df0dc309593
Natanael Beloqui de Barros <nbdb2005@gmail.com>
2025-04-15 13:40:46 -0300

    feat: adiciona objeto infoPessoais com dados do usuário

---
+++ conhecendo-objetos/1_o-que-sao-objetos/mao-na-massa/meu-objeto.js
@@ -0,0 +1,19 @@
+const infoPessoais = {
+    nome: 'Natanael',
+    sobrenome: 'Beloqui de Barros',
+    apelido: 'Top One',
+    dataDeNascimento: new Date('2005-02-20'),
+    idade: 20,
+    altura: 1.71,
+    telefone: '+5549999261533',
+    endereco: {
+        rua: 'Rua José Gomes de Freitas',
+        numero: '106',
+        complemento: 'Casa',
+        bairro: 'Costa e Silva',
+    },
+    nomeDoPai: 'Adelson Padrão Rodrigues de Barros',
+    nomeDaMae: 'Elizabeth Claudia Rodriguez Beloqui',
+    statusTrabalho: 'desempregado',
+    statusRelacionamento: 'solteiro',
+}
