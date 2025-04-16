83973729798b7430543d332d651fa58edcb5860a
Natanael Beloqui de Barros <nbdb2005@gmail.com>
2025-04-16 15:34:52 -0300

    feat: adicionar função de cálculo de média

--- conhecendo-objetos/2_manipulando-objetos/desafio-exercicios/exercicio-04.js
+++ conhecendo-objetos/2_manipulando-objetos/desafio-exercicios/exercicio-04.js
@@ -11,4 +11,17 @@ const calculadora = {
     divisao: function(a, b) {
         return b !== 0 ? a / b : null;
     },
+    calcularMedia: function(array) {
+        let total = 0;
+        array.forEach((elemento) => total += elemento);
+        return total / array.length;
+    }
 }
+
+const numeros = [1,2,3,4,5,6,7,8,9,10];
+
+console.log(calculadora.soma(9,3));
+console.log(calculadora.subtracao(9,3));
+console.log(calculadora.multiplicacao(9,3));
+console.log(calculadora.divisao(9,3));
+console.log(calculadora.calcularMedia(numeros));
