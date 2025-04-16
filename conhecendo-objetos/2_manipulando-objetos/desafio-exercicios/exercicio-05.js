abbf0caa2637a28c1e4a483dd881a89d5bc86484
Natanael Beloqui de Barros <nbdb2005@gmail.com>
2025-04-16 16:42:15 -0300

    feat: referenciando objeto dentro de objeto

---
+++ conhecendo-objetos/2_manipulando-objetos/desafio-exercicios/exercicio-05.js
@@ -0,0 +1,31 @@
+const contaBancaria = {
+    titular: 'Natanael',
+    saldo: 1000000,
+    depositar: function (valor) {
+        if (valor > 0) {
+            this.saldo += valor
+            return console.log(`R$${valor} adicionado ao saldo. Novo saldo: R$${this.saldo}`);
+        } else if (valor === 0) {
+            return console.log('Nenhum valor adicionado ao saldo.')
+        } else {
+            return console.log('Valor inválido.');
+        }
+    },
+    sacar: function (valor) {
+        if (valor >= this.saldo) {
+            this.saldo -= valor;
+            return console.log(`R$${valor} retirado do saldo. Novo saldo: R$${this.saldo}.`);
+        } else {
+            return console.log('Saldo insuficiente.');
+        }
+    }
+}
+
+const cliente = {
+    nome: 'BRKsEDU',
+    conta: contaBancaria
+}
+
+function mostrarSaldo (cliente) {
+    console.log(`O saldo do ${cliente} é ${cliente.conta.saldo}`)
+}
