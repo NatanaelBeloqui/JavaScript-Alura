5a815e1f4599b5f986d9688e8e06d82439af4e3c
Natanael Beloqui de Barros <nbdb2005@gmail.com>
2025-04-17 20:01:40 -0300

    feat: manipulando Json

--- conhecendo-objetos/4_conhecendo-o-JSON/desafio-exercicios/exercicio-04/manipulacaoJson.js
+++ conhecendo-objetos/4_conhecendo-o-JSON/desafio-exercicios/exercicio-04/manipulacaoJson.js
@@ -1,10 +1,9 @@
 const animais = require('./animais.json');
 
-console.log(animais);
+// console.log(animais);
 
 // const animaisObjeto = JSON.parse(animais); // Isso dá erro, porque o JS já faz uma conversão automática para objeto quando é importado.
-
-console.log(animaisObjeto);
+// console.log(animaisObjeto);
 
 const animalNovo = {
     "id": 4,
@@ -15,4 +14,22 @@ const animalNovo = {
 
 animais.animais.push(animalNovo)
 
-console.log(animais);
+// console.log(animais);
+
+const animalParaModificar = animais.animais.find(animal => animal.id === 2);
+if (animalParaModificar) {
+    animalParaModificar.habitat = "Oceano Antártico";
+}
+
+// console.log(animais);
+
+const indiceAnimalRemover = animais.animais.findIndex((animal) => animal.id === 1);
+if (indiceAnimalRemover !== -1) {
+    animais.animais.splice(indiceAnimalRemover, 1);
+}
+
+const animaisString = JSON.stringify(animais);
+
+// console.log(animais);
+
+console.log(animaisString);
