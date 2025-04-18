de63c6268ad2cfc78f97a3734c66583af8755fdb
Natanael Beloqui de Barros <nbdb2005@gmail.com>
2025-04-18 14:57:54 -0300

    docs: comentário sobre ordem decrescente

--- conhecendo-objetos/5_manipulando-listas-de-objetos/ordenar-estudantes.js
+++ conhecendo-objetos/5_manipulando-listas-de-objetos/ordenar-estudantes.js
@@ -9,3 +9,33 @@ function ordena(lista, propriedade) {
 }
 
 console.log(ordena(estudantes, 'nome'));
+
+
+//  DECRESCENTE
+
+// Após a ordenação da lista, podemos ajustar a implementação para inverter a ordem diretamente no método sort, considerando uma função que recebe um parâmetro indicando se a ordenação é crescente ou decrescente:
+
+// function ordenar(lista, propriedade, decrescente = false) {
+//     return lista.sort((a, b) => {
+//         let valorA = a[propriedade];
+//         let valorB = b[propriedade];
+
+//         if (typeof valorA === 'string') {
+//             valorA = valorA.toUpperCase();
+//         }
+//         if (typeof valorB === 'string') {
+//             valorB = valorB.toUpperCase();
+//         }
+
+//         if (valorA < valorB) {
+//             return decrescente ? 1 : -1;
+//         }
+//         if (valorA > valorB) {
+//             return decrescente ? -1 : 1;
+//         }
+//         return 0;
+//     });
+// }
+
+// // Exemplo de uso para ordenar de forma decrescente pelo nome
+// const ordenadoNomeDecrescente = ordenar(clientes, "nome", true);
