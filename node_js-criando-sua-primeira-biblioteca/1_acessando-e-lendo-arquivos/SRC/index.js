c2a49826c7563a19badc7efd201054eb518146ea
Natanael Beloqui de Barros <nbdb2005@gmail.com>
2025-04-21 16:26:41 -0300

    feat: adicionar função para remover pontuação das palavras

--- node_js-criando-sua-primeira-biblioteca/1_acessando-e-lendo-arquivos/SRC/index.js
+++ node_js-criando-sua-primeira-biblioteca/1_acessando-e-lendo-arquivos/SRC/index.js
@@ -17,6 +17,9 @@ fs.readFile(link, 'utf-8', (erro, texto) => {
 //     'computador': 4
 // }
 
+// Expressão regular
+// /[.,\/#!$%\^&\*;:{}=\-_`~()]/g
+
 function quebraEmParagrafos (texto) {
     const paragrafos = texto.toLowerCase().split('\n'); // Transforma o texto em uma array de string, só que por parágrafo
     const contagem = paragrafos.map((paragrafo) => {
@@ -25,12 +28,19 @@ function quebraEmParagrafos (texto) {
     console.log(contagem);
 }
 
+function limpaPalavras(palavra) {
+    return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
+  }
+
 function verificarPalavrasDuplicadas (texto) {
     const listaPalavras = texto.split(' '); // Transforma o texto em uma array de string
     const resultado = {};
     // objeto[propriedade] = valor;
     listaPalavras.forEach((palavra) => {
-        resultado[palavra] = (resultado[palavra] || 0) + 1;
+        if (palavra.length >= 3) {
+            const palavraLimpa = limpaPalavras(palavra);
+            resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) + 1;
+        }
     });
     return resultado;
 }
