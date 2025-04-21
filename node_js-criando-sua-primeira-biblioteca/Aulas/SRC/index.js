62b807406122bfedf41fad8f217bee75c20a065d
Natanael Beloqui de Barros <nbdb2005@gmail.com>
2025-04-21 18:52:04 -0300

    feat: código refatorado

--- node_js-criando-sua-primeira-biblioteca/Aulas/SRC/index.js
+++ node_js-criando-sua-primeira-biblioteca/Aulas/SRC/index.js
@@ -1,32 +1,30 @@
+// Expressão regular
+// /[.,\/#!$%\^&\*;:{}=\-_`~()]/g
+
 const fs = require('fs'); // File System
 
 const caminhoArquivo = process.argv; // Captura os argumentos passados na linha de comando ao executar o script
 const link = caminhoArquivo[2]; // "arquivos/texto.txt"
 
 fs.readFile(link, 'utf-8', (erro, texto) => {
-    quebraEmParagrafos(texto);
-    // verificarPalavrasDuplicadas(texto);
+    if (erro) {
+        console.log('Qual é o erro? ', erro.code);
+        return
+    }
+    contaPalavras (texto);
 });
 
-// criar um arrau com as palavras
-// contar as ocorrências
-// montar um objeto com o resultado
-
-// {
-//     'web': 5,
-//     'computador': 4
-// }
-
-// Expressão regular
-// /[.,\/#!$%\^&\*;:{}=\-_`~()]/g
-
-function quebraEmParagrafos (texto) {
-    const paragrafos = texto.toLowerCase().split('\n'); // Transforma o texto em uma array de string, só que por parágrafo
+function contaPalavras (texto) {
+    const paragrafos = extraiParagrafos (texto);
     const contagem = paragrafos.flatMap((paragrafo) => {
         if (!paragrafo) return [];
         return verificarPalavrasDuplicadas(paragrafo);
     })
     console.log(contagem);
+}
+
+function extraiParagrafos (texto) {
+    return texto.toLowerCase().split('\n'); // Transforma o texto em uma array de string, só que por parágrafo
 }
 
 function limpaPalavras(palavra) {
