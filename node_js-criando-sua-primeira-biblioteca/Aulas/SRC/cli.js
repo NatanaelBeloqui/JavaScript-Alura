e101d33f486095f973eeff5b0eef3f37e43449a6
Natanael Beloqui de Barros <nbdb2005@gmail.com>
2025-04-21 19:52:40 -0300

    feat: criando arquivos com fs.writeFile()

--- node_js-criando-sua-primeira-biblioteca/Aulas/SRC/cli.js
+++ node_js-criando-sua-primeira-biblioteca/Aulas/SRC/cli.js
@@ -4,12 +4,25 @@ import { contaPalavras } from './index.js';
 
 const caminhoArquivo = process.argv; // Captura os argumentos passados na linha de comando ao executar o script
 const link = caminhoArquivo[2]; // "arquivos/texto.txt"
+const endereco = caminhoArquivo[3];
 
 fs.readFile(link, 'utf-8', (erro, texto) => {
     try {
         if (erro) throw erro;
-        contaPalavras (texto);
+        const resultado = contaPalavras (texto);
+        criaESalvaArquivo(resultado, endereco);
     } catch (erro) {
         trataErros(erro);
     }
-});
+})
+
+async function criaESalvaArquivo (listaPalavras, endereco) {
+    const arquivoNovo = `${endereco}/resultado.txt`;
+    const textPalavras = JSON.stringify(listaPalavras);
+    try {
+        await fs.promises.writeFile(arquivoNovo, textPalavras);
+        console.log('Arquivo criado');
+    } catch (erro) {
+        throw erro;
+    }
+}
