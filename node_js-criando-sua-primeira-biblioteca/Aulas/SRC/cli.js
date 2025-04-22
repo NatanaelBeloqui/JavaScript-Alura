33be1b742ce1138e98c51d07fa0668cc59f15212
Natanael Beloqui de Barros <nbdb2005@gmail.com>
2025-04-21 21:55:50 -0300

    feat: adiciona função para organizar a saída de arquivos

--- node_js-criando-sua-primeira-biblioteca/Aulas/SRC/cli.js
+++ node_js-criando-sua-primeira-biblioteca/Aulas/SRC/cli.js
@@ -1,14 +1,15 @@
 import fs from 'fs';
 import trataErros from './erros/funcoesErro.js';
 import { contaPalavras } from './index.js';
+import { montaSaidaArquivo } from './helper.js';
 
 const caminhoArquivo = process.argv; // Captura os argumentos passados na linha de comando ao executar o script
-const link = caminhoArquivo[2]; // "arquivos/texto.txt"
+const link = caminhoArquivo[2];
 const endereco = caminhoArquivo[3];
 
 fs.readFile(link, 'utf-8', (erro, texto) => {
     try {
-        if (erro) throw erro;
+        if (erro) throw erro; // Lança o erro para o catch
         const resultado = contaPalavras (texto);
         criaESalvaArquivo(resultado, endereco);
     } catch (erro) {
@@ -16,13 +17,29 @@ fs.readFile(link, 'utf-8', (erro, texto) => {
     }
 })
 
-async function criaESalvaArquivo (listaPalavras, endereco) {
+// async function criaESalvaArquivo (listaPalavras, endereco) {
+//     const arquivoNovo = `${endereco}/resultado.txt`;
+//     const textoPalavras = JSON.stringify(listaPalavras);
+//     try {
+//         await fs.promises.writeFile(arquivoNovo, textPalavras);
+//         console.log('Arquivo criado');
+//     } catch (erro) {
+//         throw erro;
+//     }
+// }
+
+function criaESalvaArquivo (listaPalavras, endereco) {
     const arquivoNovo = `${endereco}/resultado.txt`;
-    const textPalavras = JSON.stringify(listaPalavras);
-    try {
-        await fs.promises.writeFile(arquivoNovo, textPalavras);
-        console.log('Arquivo criado');
-    } catch (erro) {
-        throw erro;
-    }
+    const textoPalavras = montaSaidaArquivo(listaPalavras);
+
+    fs.promises.writeFile(arquivoNovo, textoPalavras)
+        .then (() => {
+            console.log('Arquivo criado');
+        })
+        .catch ((erro) => {
+            throw erro;
+        })
+        .finally (() => console.log('Operação finalizada'))
+        // Normalmente é para encerrar conexão com o Banco de Dados
+
 }
