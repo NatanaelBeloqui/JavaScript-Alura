e7cf23cec04cdba331ef25bb40e6b597b5b6994b
Natanael <nbdb2005@gmail.com>
2025-05-30 12:57:19 -0300

    feat: organizando para fazer uma UML simples

--- programacao-orientada-a-objetos/1_entendendo-a-orientacao-a-objetos/objetoLiteral.js
+++ programacao-orientada-a-objetos/1_entendendo-a-orientacao-a-objetos/objetoLiteral.js
@@ -1,10 +1,12 @@
-const user = {
-  nome: "Juliana",
-  email: "j@j.com",
-  nascimento: "2024-01-01",
-  role: "estudante",
-  ativo: true, // por padrão
-  exibirInfos: function() {
-    console.log(this.nome, this.email);
-  }
-}
+// const user = {
+//   nome: "Juliana",
+//   email: "j@j.com",
+//   nascimento: "2024-01-01",
+//   role: "estudante",
+//   ativo: true, // por padrão
+//   exibirInfos: function() {
+//     console.log(this.nome, this.email);
+//   }
+// }
+
+// Fazer isso em um UML
---
+++ programacao-orientada-a-objetos/mao-na-massa/01/pizza.js
@@ -0,0 +1,9 @@
+const pizza =  {
+    nome: '4 queijos',
+    tamanho: 'Gigante',
+    preco: 25,
+    qtdFatias: 8,
+    bordaRecheada: false,
+
+    
+};
