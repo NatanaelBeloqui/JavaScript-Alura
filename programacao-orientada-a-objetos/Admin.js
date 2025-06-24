ed958d469b877a44c323fdd305cad188af2cd33c
Natanael <nbdb2005@gmail.com>
2025-06-24 20:42:04 -0300

    Sei lá

---
+++ programacao-orientada-a-objetos/Admin.js
@@ -0,0 +1,9 @@
+import User from "./User.js";
+
+class Admin extends User {
+    constructor(nome, email, nascimento, role = 'admin', ativo = 'true') {
+        super(nome, email, nascimento, role, ativo)
+    }
+}
+
+const novoAdmin = new Admin('Rodrigo', 'r@r.com', '2024-01-01')
--- programacao-orientada-a-objetos/mao-na-massa/01/pizza.js
+++
@@ -1,9 +0,0 @@
-const pizza =  {
-    nome: '4 queijos',
-    tamanho: 'Gigante',
-    preco: 25,
-    qtdFatias: 8,
-    bordaRecheada: false,
-
-    
-};
